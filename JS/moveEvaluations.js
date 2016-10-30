1e312b22eac116f6a9dc71f4f09600a6ba7bc987
Risitop <aziz.foucheasnoun@gmail.com>
2016-10-30 14:11:11 +0100

    Update moveEvaluations.js

--- JS/moveEvaluations.js
+++ JS/moveEvaluations.js
@@ -1,170 +1,280 @@
-PokeyI.prototype.getMaxDamageTaken = function(pokemon) {
-  // Returns [avg1, avg2, avg3, avg4, dmgTaken]
-  // where avgi is the average percentage dealt to the ennemy by our i-th move.
-  // and dmgTaken is the maximum damage dealt to our pokemon by the ennemy's one.
-
-  var name = checkExeptions(pokemon.species);
-  var setName;
-  for (setName in setdex[name])
-    if (setName)
-      break;
-
-  setName = name + " (" + setName + ")"; //Formats the set's name
-  var myPkm = new PokemonCalc(setName); //Create a formatted pokemon for the damage calculator
-  if (!myPkm)
-    return [-1, -1];
-
-  //Updates the stats with those of our pokemon
-  myPkm.maxHP = pokemon.maxhp;
-  myPkm.curHP = pokemon.hp;
-  myPkm.rawStats.at = pokemon.stats.atk;
-  myPkm.rawStats.df = pokemon.stats.def;
-  myPkm.rawStats.sa = pokemon.stats.spa;
-  myPkm.rawStats.sd = pokemon.stats.spd;
-  myPkm.rawStats.sp = pokemon.stats.spe;
-
-  myPkm.status = statusConvert(pokemon.stats.spe); //Formats the status
-  myPkm.item = BattleItems[pokemon.item]; //Gets and formats the item
-
-  myPkm.ability = BattleAbilities[pokemon.baseAbility].name; //formats the ability
-  this.applyBoosts(myPkm, pokemon); //Transfers eventual boosts
-
-  for (var i = 0; i < pokemon.moves.length; i++) { //Goes through our moves
-    var m = Moves[BattleMovedex[pokemon.moves[i]].name]; //Formats the ith move
-    if (m) { //Checks for exceptions and assigns the move
-      m.bp = this.getBpException(BattleMovedex[pokemon.moves[i]].name);
-      myPkm.moves[i] = m;
-    } else
-      myPkm.moves[i] = Moves["(No Move)"];
-  }
-
-  // We look for the maximum amount of damage we can take
-
-  var maxiDmg = [0, 0, 0, 0, 0];
-  var f = new Field();
-  var numberOfSets = 0;
-  var ennemyName = checkExeptions(this.bot.ennemy.species); //Gets the name of the opponent's pokemon
-  for (var set in setdex[ennemyName]) { //Goes through its possible sets
-    setName = ennemyName + " (" + set + ")"; //formats the set's name
-    var ennemyPkm = new PokemonCalc(setName); //Creates a pokemon with the appropriate set
-
-    var dmg = calculateAllMoves(ennemyPkm, myPkm, f);
-    /*a 2*4*16 array containing the dmg of the moves 
-                                                           for each variance level of both pokemons */
-
-    for (var i = 0; i < dmg[1].length; i++) { //Goes through the damage that the ennemy can deal us
-      if (dmg[0][i].damage[15] > maxiDmg[4]) // false if it does not exist
-        maxiDmg[4] = dmg[0][i].damage[15]; //Contains the maximum damage we can possibly take
+Risibot.prototype.choseMove = function() {
+    
+    if (!this.pokemon || !this.ennemy)
+        return -1;
+    
+    var dmgComputation = this.AI.getMaxDamageTaken(this.pokemon, this.ennemy);
+    var dmgTaken = dmgComputation[4];
+    var movesInterests = dmgComputation.slice(0, 4);
+    
+    for (var i = 0; i < dmgTaken.length; i++) {
+      if (dmgTaken[i] && dmgTaken[i] != NaN)
+        dmgTaken[i] = 100 - Math.min(100, this.ennemy.hp - dmgTaken[i]);
     }
-
-    for (var i = 0; i < dmg[0].length; i++) { //Goes trough the damage dealt to the ennemy
-      if (ennemyPkm.level == 100) {
-        numberOfSets += 1;
-        maxiDmg[i] += dmg[1][i].damage[7]; //Contains the average damage dealt to the ennemy by our ith move
-      }
+    
+    for (var moveType in this.moves) {
+        for (var j = 0; j < this.moves[moveType].length; j++) {
+            var move = this.moves[moveType][j][0];
+            var k = this.moves[moveType][j][1];
+            switch (moveType) {
+                case 'status':
+                    movesInterests[k - 1] = this.AI.evalStatus(move, dmgTaken);
+                    break;
+                case 'traps':
+                    movesInterests[k - 1] = this.AI.evalTraps(move, dmgTaken);
+                    break;
+                case 'heal':
+                    movesInterests[k - 1] = this.AI.evalHeal(move, dmgTaken);
+                    break;
+                case 'spin':
+                    movesInterests[k - 1] = this.AI.evalSpin(move, dmgTaken);
+                    break;
+                case 'seeds':
+                    movesInterests[k - 1] = this.AI.evalSeeds(move, dmgTaken);
+                    break;
+                case 'defog':
+                    movesInterests[k - 1] = this.AI.evalDefog(move, dmgTaken);
+                    break;
+                case 'roar':
+                    movesInterests[k - 1] = this.AI.evalRoar(move, dmgTaken);
+                    break;
+                case 'painSplit':
+                    movesInterests[k - 1] = this.AI.evalPainSplit(move, dmgTaken);
+                    break;
+            }
+            console.log("Risibot: choseMove: Move " + move.name + ".");
+        }
     }
-  }
-
-  for (var i = 0; i < pokemon.moves.length; i++) { //Corner cases
-    switch (pokemon.moves[i]) {
-      case "sonicboom":
-        maxiDmg[i] = 20;
-        break;
-      case "dragonrage":
-        maxiDmg[i] = 40;
-        break;
-      case "seismictoss":
-      case "nightshade":
-        maxiDmg[i] = pokemon.level;
-        break;
-      case "finalgambit":
-        maxiDmg[i] = pokemon.maxhp;
-        break;
+    // We delete disabled moves (scarf, disable...)
+    var disabledMoves = [1, 1, 1, 1];
+    for (var j = 0; j < this.buttonsMoves.length; j++) {
+        if (this.buttonsMoves[j])
+            disabledMoves[this.buttonsMoves[j].value - 1] = 0;
+    }       
+    // We erase the NaN and disabled moves
+    for (j = 0; j < 4; j++) {
+        movesInterests[j] = ( (disabledMoves[j] || movesInterests[j] == NaN) ? 0 : movesInterests[j] );
     }
-    maxiDmg[i] /= numberOfSets; // We calculate the average power
-    //Transforms the values in life loss percentage
-    maxiDmg[i] = parseInt(100 * maxiDmg[i] / this.bot.ennemy.maxhp);
-    //Refines the calculation by taking into account critical hits
-    maxiDmg[i] = parseInt(maxiDmg[i] * (15 / 16) + maxiDmg[i] * (1 / 16) * ((myPkm.ability == "Sniper") ? 2 : 1.5)); // E(X) with critical
-  }
-  //Transforms the value in life loss percentage
-  maxiDmg[4] = parseInt(maxiDmg[4] / this.bot.pokemon.maxhp * 100);
-  //Takes into account critical hits
-  maxiDmg[4] = parseInt(maxiDmg[4] * (15 / 16) + maxiDmg[4] * (1 / 16) * ((this.hasAbility(this.bot.ennemy, "Sniper")) ? 2 : 1.5)); // E(X) with critical
-
-  for (var i = 0; i < 4; i++) { // 100 - remaining hp
-    maxiDmg[i] = Math.min(100, this.bot.ennemy.hp - maxiDmg[i]);
-  }
-
-  return maxiDmg;
+    console.log("Risibot: choseMove: " + movesInterests); 
+    var choice = getMaxIndex(movesInterests);
+    return choice;
 };
 
-statusConvert = function(st) { //Formats Showdown's statuses for the Damage Calculator
+PokeyI.prototype.evalStatus = function(move, dmgTaken) { // Is this status move worth it ?
 
-  switch (status) {
-    case "brn":
-      return "Burned";
-    case "psn":
-      return "Poisoned";
-    case "tox":
-      return "Badly Poisoned";
-    case "slp":
-      return "Asleep";
-    case "frz":
-      return "Frozen";
+  if (this.bot.ennemy.status != "" ||
+    (move.status == "par" && this.hasType(this.bot.ennemy, "Electric")) ||
+    (move.status == "brn" && this.hasType(this.bot.ennemy, "Fire")) ||
+    (move.baseType == "Grass" && this.hasType(this.bot.ennemy, "Grass")) ||
+		(move.baseType == "Electric" && this.hasType(this.bot.ennemy, "Ground")) ||
+		(move.baseType == "tox" && this.hasType(this.bot.ennemy, "Steel")) ||
+    (move.status == "tox" && this.hasType(this.bot.ennemy, "Poison")) ||
+		(move.status == "par" && this.hasAbility(this.bot.ennemy, "Limber")) ||
+		(move.status == "brn" && this.hasAbility(this.bot.ennemy, "Water Veil")) ||
+    (this.hasAbility(this.bot.ennemy, "Magic Bounce")) ||
+    (this.hasAbility(this.bot.ennemy, "Synchronize")) ||
+		(this.room.battle.yourSide.sideConditions.safeguard)) {
+    return 0;
   }
-  return "Healthy";
+	
+	var isAnnoying = this.getStatusInterest(move.status) / ( (this.canCure(this.bot.ennemy)) ? 2 : 1) ;				
+	return parseInt( ((isAnnoying + 1) * 30) ); // At most 90. Healing will be greater.
 };
 
-PokeyI.prototype.getBpException = function(moveName) { //Calculate the base power of some special attacks
+PokeyI.prototype.getStatusInterest = function(s) {
+	switch (s) {
+		case 'par':
+			if (this.bot.ennemy.baseStats["spe"] > 120 || this.bot.ennemy.item == "Choice Scarf")
+				return 2;
+			if (this.bot.ennemy.baseStats["spe"] > 100)
+				return 1;
+			return 0;
+		case 'tox':
+			var wallCoef = this.getPotentialWall(this.bot.ennemy);
+			return ( (wallCoef >= 20) ? 2 : (wallCoef >= 15) ? 1 : 0 );
+		case 'brn':
+			if (this.bot.ennemy.baseStats["atk"] > 120 || this.bot.ennemy.item == "Choice Band")
+				return 2;
+			if (this.bot.ennemy.baseStats["atk"] > 100)
+				return 1;
+			return 0;
+		case 'slp':
+			return 1;
+	}
+	return 0;
+}
 
-  var bp = Moves[moveName].bp;
+PokeyI.prototype.evalSpin = function(move, dmgTaken) { // Is this spin worth it ?
 
-  switch (moveName) {
-    case "Low Kick":
-    case "Grass Knot":
-      w = this.bot.ennemy.weightkg;
-      bp = (w < 10) ? 20 : (w < 25) ? 40 : (w < 50) ? 60 : (w < 100) ? 80 : (w < 200) ? 100 : 120;
-      break;
-    case "Heavy Slam":
-      r = this.bot.pokemon.weightkg / this.bot.ennemy.weightkg;
-      bp = (r > 1 / 2) ? 40 : (r > 1 / 3) ? 60 : (r > 1 / 4) ? 80 : (r > 1 / 5) ? 100 : 120;
-      break;
-    case "Gyro Ball":
-      ennemySpeed = parseInt((31 + 2 * this.bot.ennemy.baseStats.spe) * this.bot.ennemy.level / 100 + 5);
-      bp = 25 * (ennemySpeed / this.bot.pokemon.stats.spe);
-      break;
-    case "Facade":
-      bp = 70 * ((this.bot.pokemon.status == "") ? 1 : 2);
-      break;
-    case "Return":
-      bp = 102;
-      break;
-    case "Frustration":
-      bp = 102;
-      break;
+  if ((jQuery.isEmptyObject(this.bot.room.battle.mySide.sideConditions) && !this.bot.pokemon.volatiles.leechseed) ||
+    this.hasType(this.bot.ennemy, "Ghost")) {
+    return 0;
   }
-  return bp;
+  return 150;
 };
 
-PokeyI.prototype.applyBoosts = function(pkmCalc, pkm) { //Converts the Showdown's representation into the Dmg Calc one
-  for (var b in pkmCalc.boosts) {
-    switch (b) {
-      case "at":
-        pkmCalc.at = (!pkm.boosts.atk) ? 0 : pkm.boosts.atk;
+PokeyI.prototype.evalTraps = function(move) {
+
+  if (this.getDanger(this.bot.ennemy, this.bot.pokemon) > 100 || this.hasAbility(this.bot.ennemy, "Magic Bounce"))
+    return 0;
+
+  switch (move.id) {
+    case "stealthrock":
+      if (!this.bot.room.battle.yourSide.sideConditions.stealthrock)
+        return 150;
+      break;
+    case "spikes":
+      if (!this.bot.room.battle.yourSide.sideConditions.spikes)
+        return 150;
+      break;
+    case "stickyweb":
+      if (!this.bot.room.battle.yourSide.sideConditions.stickyweb)
+        return 150;
+      break;
+  }
+  console.log("Risibot: evalTraps: Entry hazards are already set.");
+  return 0;
+};
+
+PokeyI.prototype.evalHeal = function(move) {
+
+  hp = this.bot.pokemon.hp / this.bot.pokemon.hpmax;
+  if (hp < 0.25)
+    return 1500;
+  if (hp < 0.50)
+    return 1000;
+  if (hp < 0.60)
+    return 500;
+  if (hp < 0.75)
+    return 100;
+  if (hp < 0.90)
+    return 30;
+  return 0;
+};
+
+PokeyI.prototype.evalSeeds = function(move) {
+
+  if (this.bot.ennemy.volatiles.leechseed || this.hasType(this.bot.ennemy, "Grass") || this.hasAbility(this.bot.ennemy, "Magic Bounce"))
+    return 0;
+
+  var d = this.getDanger(this.bot.ennemy, this.bot.pokemon);
+  if (d < 0.3) // Ennemy could switch
+    return 100;
+  if (d < 0.8) // Ennemy will probably not switch
+    return 300;
+  if (d < 1.5) // It's crucial to survive
+    return 399;
+  return 0 // ABORT MISSION
+};
+
+PokeyI.prototype.evalDefog = function(move) {
+
+  if (!this.bot.room.battle.mySide.sideConditions)
+    return 0;
+
+  var indic = 0;
+  for (var condition in this.bot.room.battle.mySide.sideConditions) {
+    var c = this.bot.room.battle.mySide.sideConditions[condition][0];
+    switch (c) {
+      case 'toxicspikes':
+      case 'spikes':
+        indic += 1;
         break;
-      case "df":
-        pkmCalc.df = (!pkm.boosts.def) ? 0 : pkm.boosts.def;
+      case 'stickyweb':
+      case 'stealthrock':
+        indic += 2;
         break;
-      case "sa":
-        pkmCalc.sa = (!pkm.boosts.spa) ? 0 : pkm.boosts.spa;
-        break;
-      case "sd":
-        pkmCalc.sd = (!pkm.boosts.spd) ? 0 : pkm.boosts.spd;
-        break;
-      case "sp":
-        pkmCalc.sp = (!pkm.boosts.spe) ? 0 : pkm.boosts.spe;
+      case 'reflect':
+      case 'safeguard':
+      case 'lightscreen':
+        indic -= 2;
         break;
     }
   }
+  for (var condition in this.bot.room.battle.yourSide.sideConditions) {
+    var c = this.bot.room.battle.yourSide.sideConditions[condition][0];
+    switch (c) {
+      case 'toxicspikes':
+      case 'spikes':
+        indic -= 1;
+        break;
+      case 'stickyweb':
+      case 'stealthrock':
+        indic -= 2;
+        break;
+      case 'reflect':
+      case 'safeguard':
+      case 'lightscreen':
+        indic += 2;
+        break;
+    }
+  }
+
+  if (indic < 0) // Terrain is good for the bot
+    return 0;
+  else
+    return 50 * indic;
+};
+
+PokeyI.prototype.evalRoar = function(move) {
+
+  if (this.hasAbility(this.bot.ennemy, "Magic Bounce"))
+    return 0;
+
+  coef = 1.0;
+  for (c in this.bot.room.battle.yourSide.sideConditions) {
+    switch (this.bot.room.battle.yourSide.sideConditions[c]) {
+      case 'stealthrock': // Will damage and destabilize
+      case 'spikes':
+      case 'toxicspikes':
+      case 'reflect': // Will temporize
+      case 'lightscreen':
+      case 'safeguard':
+        coef += 1.0;
+    }
+  }
+  for (b in this.bot.ennemy.boosts) {
+    coef += this.bot.ennemy.boosts[b];
+  }
+
+  coef = Math.max(0.0, coef - this.getDanger(this.bot.ennemy, this.bot.pokemon));
+
+  return 70 * coef;
+
+};
+
+
+/////////////////// UNSSAFE ZONE //////////////////////////////////////
+
+PokeyI.prototype.evalPainSplit = function(move) { // NOT TESTED NOW
+
+  expectedDamage = this.getMaxDamageTaken(this.bot.pokemon, this.bot.ennemy)[1];
+  ennemyHP = parseInt((parseInt(31 + 2 * this.bot.ennemy.baseStats * this.bot.ennemy.level / 100) + 10 + this.bot.ennemy.level) * this.bot.ennemy.hp / 100);
+
+  if (this.isFaster(this.bot.pokemon, this.bot.ennemy)) { // If I should hit first
+    newHP = parseInt((ennemyHP + this.bot.pokemon.hp) / 2); // Hp after pain split
+
+    if (newHP < this.bot.pokemon.hp) // We will lose HP
+      return 0;
+
+    if (newHP < expectedDamage) { // We will probably die
+      ratio = (this.bot.pokemon.hp / this.bot.pokemon.maxhp);
+      return (ratio < 0.25) ? 100 : (ratio < 0.5) ? 50 : 0;
+    }
+
+    return (newHP / this.bot.pokemon.hp) * 100;
+  }
+
+  // If I will hit in second
+
+  expectedHP = this.bot.pokemon.hp - expectedDamage;
+  if (expectedHp < 0) // We will die before having done anything
+    return 150; // FINAL GAMBIT
+
+  newHP = parseInt((ennemyHP + expectedHP) / 2);
+
+  if (newHP < expectedHP) // We will lose HP
+    return 0;
+
+  return (newHP / expectedHP) * 100;
 };
